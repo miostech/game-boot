80350c857d44cf2f398c51c835ec2d3b8fd864b9
Oscar Augusto <oas-desenvolvimento@outlook.com>
2023-10-02 17:22:12 +0100

    fixed

--- get_users_local.py
+++ get_users_local.py
@@ -7,30 +7,33 @@ def get_users():
     config_users = []
     id = 1
     for item in range(1, 61):
-        print(item)
-        info_slot = read_file_cfg.return_all_info_user_slot(item)
+        try:
+            print(item)
+            info_slot = read_file_cfg.return_all_info_user_slot(item)
 
-        info_last, info_first = update_registry.return_last_auth_and_first_char("slot" + str(item))
+            info_last, info_first = update_registry.return_last_auth_and_first_char("slot" + str(item))
 
-        info_proxy = read_ppx.return_proxy_by_idx(item)
-        print(info_slot)
-        print(info_last)
-        print(info_first)
-        print(info_proxy)
-        if info_slot["mail"] is not None or info_slot["mail"] != "":
-            config_users.append({
-                "id": 0,
-                "used": 0,
-                "email": info_slot["mail"],
-                "password": info_slot["pass"],
-                "account": info_slot["account"],
-                "server": 6,
-                "first_char": info_first[0],
-                "last_auth": info_last[0],
-                "password_proxy": info_proxy,
-                "logs": [],
-                "total_cor": 0
-            })
+            info_proxy = read_ppx.return_proxy_by_idx(item)
+            print(info_slot)
+            print(info_last)
+            print(info_first)
+            print(info_proxy)
+            if info_slot["mail"] is not None or info_slot["mail"] != "":
+                config_users.append({
+                    "id": 0,
+                    "used": 0,
+                    "email": info_slot["mail"],
+                    "password": info_slot["pass"],
+                    "account": info_slot["account"],
+                    "server": 6,
+                    "first_char": info_first[0],
+                    "last_auth": info_last[0],
+                    "password_proxy": info_proxy,
+                    "logs": [],
+                    "total_cor": 0
+                })
+        except Exception as ex:
+            print(ex)
     data_save = json.dumps(config_users, indent=4)
 
     with open("accounts_logs.json", "w") as r:
